3d07bacd7ea0014aa5db60adead64b940701b0c6
jdpc-hub <jdpc20392@gmail.com>
2020-11-02 11:17:20 -0500

    Se corrige la sangría de los archivos.

--- agendaDeContactos/Agenda.java
+++ agendaDeContactos/Agenda.java
@@ -4,141 +4,141 @@ import java.util.Scanner;
 
 class Agenda {
 
-	//Variables de instancia
-	private Contacto[] contacto;
+    //Variables de instancia
+    private Contacto[] contacto;
 
-	//Constructor
-	Agenda(int cantidad) {
-		contacto = new Contacto[cantidad];
-		System.out.println("Se ha creado una agenda con capacidad para "+cantidad+" contactos.\n");
-	}
+    //Constructor
+    Agenda(int cantidad) {
+        contacto = new Contacto[cantidad];
+        System.out.println("Se ha creado una agenda con capacidad para "+cantidad+" contactos.\n");
+    }
 
-	/////////////
-	// Métodos //
-	/////////////
-	
-	/**
-	 * Busca una posición disponible para agregar un contacto. Si la agenda está
-	 * llena, el método lanza una excepción que contiene un mensaje diciendo que
-	 * la agenda está llena.
-	 * */
-	private int posDisponible() throws Exception {
-		int i;
-		for (i = 0; i < contacto.length; i++) {
-			if (contacto[i] == null) {
-				return i;
-			}
-		}
-		throw new Exception("La agenda está llena");
-	}
-	
-	/**
-	 * Añade un contacto a la lista de cotactos.
-	 */
-	void addContacto(Contacto nuevoContacto) {
-		int i;
-		try {
-			i = posDisponible();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			return;
-		}
-		contacto[i] = nuevoContacto;
-		System.out.println("Un cotacto ha sido agregado a la agenda.\n");
-	}
-	
-	void addContacto() {
-		int i;
-		try {
-			i = posDisponible();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			return;
-		}
-		Scanner entrada = new Scanner(System.in);
-		String nombre, apellidos, email, telefono, direccion, tipoContacto;
-		System.out.print("Ingrese el nombre del nuevo contacto: ");
-		nombre = entrada.nextLine();
-		System.out.print("Ingrese los apellidos del nuevo contacto: ");
-		apellidos = entrada.nextLine();
-		System.out.print("Ingrese el email del nuevo contacto: ");
-		email = entrada.nextLine();
-		System.out.print("Ingrese el telefono del nuevo contacto: ");
-		telefono = entrada.nextLine();
-		System.out.print("Ingrese la dirección del nuevo contacto: ");
-		direccion = entrada.nextLine();
-		System.out.print("¿Qué tipo de contacto es? ");
-		tipoContacto = entrada.nextLine();
-		Contacto nuevoContacto = new Contacto(nombre, apellidos, email, telefono, direccion, tipoContacto);
-		contacto[i] = nuevoContacto;
-		System.out.println("\nUn cotacto ha sido agregado a la agenda.\n");
-		entrada.close();
-	}
+    /////////////
+    // Métodos //
+    /////////////
 
-	void mostrarContacto(int i) {
-		System.out.println(
-			contacto[i].getNombre()+" "+contacto[i].getApellido()+"\n"+
-			"    id: "+i+"\n"+
-			"    Telefono: "+contacto[i].getTelefono()+"\n"+
-			"    Email: "+contacto[i].getEmail()+"\n"+
-			"    Dirección: "+contacto[i].getDireccion()+"\n"+
-			"    Tipo de contacto: "+contacto[i].getTipoContacto()+"\n");
-	}
+    /**
+    * Busca una posición disponible para agregar un contacto. Si la agenda está
+    * llena, el método lanza una excepción que contiene un mensaje diciendo que
+    * la agenda está llena.
+    * */
+    private int posDisponible() throws Exception {
+        int i;
+        for (i = 0; i < contacto.length; i++) {
+            if (contacto[i] == null) {
+                return i;
+            }
+        }
+        throw new Exception("La agenda está llena");
+    }
 
-	void listarContactos() {
-		System.out.println("Lista de contactos:\n");
-		if (contacto[0] == null) {
-			System.out.println("---> La agenda está vacía.\n");
-			return;
-		}
-		for (int i = 0; i < contacto.length && contacto[i] != null; i++) {
-			mostrarContacto(i);
-		}
-	}
+    /**
+    * Añade un contacto a la lista de cotactos.
+    */
+    void addContacto(Contacto nuevoContacto) {
+        int i;
+        try {
+            i = posDisponible();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        contacto[i] = nuevoContacto;
+        System.out.println("Un cotacto ha sido agregado a la agenda.\n");
+    }
 
-	void eliminarContacto() {
-		if (contacto[0] == null) {
-			System.out.println("---> La agenda está vacía.\n");
-			return;
-		}
-		System.out.print("Ingrese el id del contacto a eliminar: ");
-		Scanner entrada = new Scanner(System.in);
-		int i = entrada.nextInt();
-		entrada.close();
-		try {
-			if (contacto[i] == null) {
-				System.out.println("---> No existe un contacto con ese id.\n");
-				return;
-			}
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("---> No existe un contacto con ese id.\n");
-			return;
-		}
-		int j;//Índice del último contacto en la lista
-		try {
-			j = posDisponible() - 1;
-		} catch (Exception e) {
-			j = contacto.length - 1;
-		}
-		contacto[i] = contacto[j];
-		contacto[j] = null;
-		System.out.println("\nSe ha eliminado un contacto.\n");
-	}
-	
-	void buscarPorNombre() {
-		System.out.print("Ingrese el nombre del contacto que busca: ");
-		Scanner entrada = new Scanner(System.in);
-		String nombre = entrada.nextLine();
-		entrada.close();
-		int contador = 0;
-		for (int i = 0; i < contacto.length && contacto[i] != null; i++) {
-			if (contacto[i].getNombre().toLowerCase().contains(nombre.toLowerCase())) {
-				mostrarContacto(i);
-				contador++;
-			}
-		}
-		if (contador == 0) {
-			System.out.println("---> No se encontró ninguna coincidencia.");
-		}
-	}
+    void addContacto() {
+        int i;
+        try {
+            i = posDisponible();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        Scanner entrada = new Scanner(System.in);
+        String nombre, apellidos, email, telefono, direccion, tipoContacto;
+        System.out.print("Ingrese el nombre del nuevo contacto: ");
+        nombre = entrada.nextLine();
+        System.out.print("Ingrese los apellidos del nuevo contacto: ");
+        apellidos = entrada.nextLine();
+        System.out.print("Ingrese el email del nuevo contacto: ");
+        email = entrada.nextLine();
+        System.out.print("Ingrese el telefono del nuevo contacto: ");
+        telefono = entrada.nextLine();
+        System.out.print("Ingrese la dirección del nuevo contacto: ");
+        direccion = entrada.nextLine();
+        System.out.print("¿Qué tipo de contacto es? ");
+        tipoContacto = entrada.nextLine();
+        Contacto nuevoContacto = new Contacto(nombre, apellidos, email, telefono, direccion, tipoContacto);
+        contacto[i] = nuevoContacto;
+        System.out.println("\nUn cotacto ha sido agregado a la agenda.\n");
+        entrada.close();
+    }
+
+    void mostrarContacto(int i) {
+        System.out.println(
+        contacto[i].getNombre()+" "+contacto[i].getApellido()+"\n"+
+        "    id: "+i+"\n"+
+        "    Telefono: "+contacto[i].getTelefono()+"\n"+
+        "    Email: "+contacto[i].getEmail()+"\n"+
+        "    Dirección: "+contacto[i].getDireccion()+"\n"+
+        "    Tipo de contacto: "+contacto[i].getTipoContacto()+"\n");
+    }
+
+    void listarContactos() {
+        System.out.println("Lista de contactos:\n");
+        if (contacto[0] == null) {
+            System.out.println("---> La agenda está vacía.\n");
+            return;
+        }
+        for (int i = 0; i < contacto.length && contacto[i] != null; i++) {
+            mostrarContacto(i);
+        }
+    }
+
+    void eliminarContacto() {
+        if (contacto[0] == null) {
+            System.out.println("---> La agenda está vacía.\n");
+            return;
+        }
+        System.out.print("Ingrese el id del contacto a eliminar: ");
+        Scanner entrada = new Scanner(System.in);
+        int i = entrada.nextInt();
+        entrada.close();
+        try {
+            if (contacto[i] == null) {
+                System.out.println("---> No existe un contacto con ese id.\n");
+                return;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("---> No existe un contacto con ese id.\n");
+            return;
+        }
+        int j;//Índice del último contacto en la lista
+        try {
+            j = posDisponible() - 1;
+        } catch (Exception e) {
+            j = contacto.length - 1;
+        }
+        contacto[i] = contacto[j];
+        contacto[j] = null;
+        System.out.println("\nSe ha eliminado un contacto.\n");
+    }
+
+    void buscarPorNombre() {
+        System.out.print("Ingrese el nombre del contacto que busca: ");
+        Scanner entrada = new Scanner(System.in);
+        String nombre = entrada.nextLine();
+        entrada.close();
+        int contador = 0;
+        for (int i = 0; i < contacto.length && contacto[i] != null; i++) {
+            if (contacto[i].getNombre().toLowerCase().contains(nombre.toLowerCase())) {
+                mostrarContacto(i);
+                contador++;
+            }
+        }
+        if (contador == 0) {
+            System.out.println("---> No se encontró ninguna coincidencia.");
+        }
+    }
 }
--- agendaDeContactos/Main.java
+++ agendaDeContactos/Main.java
@@ -3,79 +3,79 @@ package agendaDeContactos;
 import java.util.Scanner;
 
 class Main {
-	private static Scanner entrada;
+    private static Scanner entrada;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Contacto contac1 = new Contacto("Carlos David",
-				"Posada Soto",
-				"carlangas@correo.com",
-				"3127289032",
-				"cll 30 sur # 54-94",
-				"personal");
+        Contacto contac1 = new Contacto("Carlos David",
+        "Posada Soto",
+        "carlangas@correo.com",
+        "3127289032",
+        "cll 30 sur # 54-94",
+        "personal");
 
-		Contacto contac2 = new Contacto("Isabel Cristina",
-				"Ramírez Correa",
-				"isabelc@correo.com",
-				"3127789034",
-				"cll 10 # 56-13, garage",
-				"personal");
+        Contacto contac2 = new Contacto("Isabel Cristina",
+        "Ramírez Correa",
+        "isabelc@correo.com",
+        "3127789034",
+        "cll 10 # 56-13, garage",
+        "personal");
 
-		Contacto contac3 = new Contacto("Sebastián",
-				"Gómez Ardila",
-				"sgomez@correo.com",
-				"3225052356",
-				"cll 100 # 34-23",
-				"empresa");
-		Agenda agenda = new Agenda(10);
+        Contacto contac3 = new Contacto("Sebastián",
+        "Gómez Ardila",
+        "sgomez@correo.com",
+        "3225052356",
+        "cll 100 # 34-23",
+        "empresa");
+        Agenda agenda = new Agenda(10);
 
-		agenda.addContacto(contac1);
-		agenda.addContacto(contac2);
-		agenda.addContacto(contac3);
+        agenda.addContacto(contac1);
+        agenda.addContacto(contac2);
+        agenda.addContacto(contac3);
 
-		mostrarMenu();
-		entrada = new Scanner(System.in);
-		int opcion;
-		boolean entrar = true;
-		while (entrar) {
-			System.out.print(">>> ");
-			opcion = entrada.nextInt();
+        mostrarMenu();
+        entrada = new Scanner(System.in);
+        int opcion;
+        boolean entrar = true;
+        while (entrar) {
+            System.out.print(">>> ");
+            opcion = entrada.nextInt();
 
-			switch (opcion) {
-			case 1:
-				agenda.addContacto();
-				break;
-			case 2:
-				agenda.listarContactos();
-				break;
-			case 3:
-				agenda.eliminarContacto();
-				break;
-			case 4:
-				agenda.buscarPorNombre();
-				break;
-			case 5:
-				mostrarMenu();
-				break;
-			default:
-				System.out.println("Hasta luego. :D");
-				entrar = false;
-				break;
-			}
-		}
-	}
+            switch (opcion) {
+                case 1:
+                agenda.addContacto();
+                break;
+                case 2:
+                agenda.listarContactos();
+                break;
+                case 3:
+                agenda.eliminarContacto();
+                break;
+                case 4:
+                agenda.buscarPorNombre();
+                break;
+                case 5:
+                mostrarMenu();
+                break;
+                default:
+                System.out.println("Hasta luego. :D");
+                entrar = false;
+                break;
+            }
+        }
+    }
 
-	private static void mostrarMenu() {
-		System.out.print(
-			"**************************\n"+
-			"*          MENÚ          *\n"+
-			"**************************\n\n"+
-			"Ingrese:\n"+
-			"   1 para crear un contacto.\n"+
-			"   2 para listar los contactos.\n"+
-			"   3 para eliminar un contacto.\n"+
-			"   4 para buscar por nombre.\n"+
-			"   5 para volver a mostrar este menú.\n"+
-			"   Cualquier otro número para salir.\n\n");
-	}
+    private static void mostrarMenu() {
+        System.out.print(
+        "**************************\n"+
+        "*          MENÚ          *\n"+
+        "**************************\n\n"+
+        "Ingrese:\n"+
+        "   1 para crear un contacto.\n"+
+        "   2 para listar los contactos.\n"+
+        "   3 para eliminar un contacto.\n"+
+        "   4 para buscar por nombre.\n"+
+        "   5 para volver a mostrar este menú.\n"+
+        "   Cualquier otro número para salir.\n\n");
+    }
 }
